b12b0c09053638f64b06e632f54f22f4b544bd7a
Mohammadyasin246 <157456080+Mohammadyasin246@users.noreply.github.com>
2024-08-07 14:08:13 +0530

    Update turtle demo.py

--- turtle demo.py
+++ turtle demo.py
@@ -1,12 +1,1 @@
-import turtle
 
-# Create turtle object
-t = turtle.Turtle()
-
-# Draw a square
-for _ in range(100000000000):
-    t.forward(100)  # Move the turtle forward by 100 units
-    t.left(45)    # Turn the turtle left by 90 degrees
-
-# Close the turtle graphics window on click
-# turtle.exitonclick()
